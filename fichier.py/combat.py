b2c512061569dd3a32a45ea2ec7f4a9dc907aee9
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2024-01-12 14:36:14 +0100

    3

--- fichier.py/combat.py
+++ fichier.py/combat.py
@@ -3,5 +3,10 @@ class Combat :
     def __init__(self , joueur , adversaie):
         self.joueur  = joueur
         self.adversaie = adversaie
-    def enlève_points(joueur , advairsaire):
+    def enlève_points(type_joueur , attaque , défense):
+        multiplication_dégats  = 0,5
+        return int(attaque * multiplication_dégats)
+    
+    
+
         
