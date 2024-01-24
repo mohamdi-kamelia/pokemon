229089489e3a0a410f7e717db6770162bc999631
SOPHIA FRAOUET <147490310+sofia-fraouet@users.noreply.github.com>
2024-01-24 11:39:54 +0100

    sauvegarde

--- p.py
+++ p.py
@@ -1,8 +1,7 @@
 import json
 
-
 class Pokedex:
-    def __init__(self, save:str=None):
+    def __init__(self, save: str = None):
         if save is None:
             self.data = [None] * 24
             with open("pokedex.json", 'r', encoding='utf-8') as f:
@@ -10,44 +9,29 @@ class Pokedex:
                 for k in range(18, 24):
                     self.data[k] = pokdata[k]
             self.save_dex()
-    
+        else:
+            with open(save, 'r', encoding='utf-8') as f:
+                self.data = json.load(f)
+
     def read_dex(self):
         return self.data
-    
+
     def add_pokemon(self, id_pok: int):
-        if isinstance(id_pok, int):
+        if isinstance(id_pok, int) and 0 < id_pok <= 24:
             with open('pokedex.json', "r", encoding='utf-8') as f:
                 pokdata = json.load(f)
                 self.data[id_pok] = pokdata[id_pok]
             self.save_dex()
-    
-    def save_dex(self):
-        with open('pokemon.json', 'w') as f:
-                json.dump(self.data, f, indent=2)
 
+    def save_dex(self):
+        with open('pokedex.json', 'w') as f:
+            json.dump(self.data, f, indent=2)
 
 if __name__ == '__main__':
     dex = Pokedex()
 
+    # Exemple : Ajouter un Pokémon avec l'ID 5
+    dex.add_pokemon(5)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Afficher le Pokédex mis à jour
+    print(dex.read_dex())
--- pokedex.py
+++ pokedex.py
@@ -3,20 +3,27 @@ import json
 
 class Pokedex:
     def __init__(self, save:str=None):
+        self.data = [None] * 24
         if save is None:
-            self.data = [None] * 24
             with open("pokedex.json", 'r', encoding='utf-8') as f:
                 pokdata = json.load(f)
                 for k in range(18, 24):
                     self.data[k] = pokdata[k]
             self.save_dex()
+        else:
+            with open(save, 'r', encoding='utf-8') as f:
+                self.data = json.load(f)
     
     def read_dex(self):
-        return self.data
+        data = []
+        for k in range(len(self.data)):
+            if self.data[k] is not None:
+                data.append(self.data[k])
+        return data
     
     def add_pokemon(self, id_pok: int):
         if isinstance(id_pok, int):
-            with open('pokedex.json', r, encoding='utf-8') as f:
+            with open('pokedex.json', 'r', encoding='utf-8') as f:
                 pokdata = json.load(f)
                 self.data[id_pok] = pokdata[id_pok]
             self.save_dex()
@@ -28,5 +35,10 @@ class Pokedex:
 
 if __name__ == '__main__':
     dex = Pokedex()
+    dexc = Pokedex(save='pokedex.json')
+    dex.add_pokemon(1)
+    # Code pour utiliser la sauvegarde dex = Pokedex(save='pokemon.json')
+    print(dex.read_dex())
+
 
 
