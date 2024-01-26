31bb340d48013045e3d40af33eb20c2717ea0f9b
maysa-bik <maysa.bik@laplateforme.com>
2024-01-26 03:03:19 +0100

    faire le map

---
+++ Map.py/house2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="house2" tilewidth="16" tileheight="16" tilecount="1210" columns="55">
+ <image source="house2.png" trans="ffffff" width="894" height="359"/>
+</tileset>
