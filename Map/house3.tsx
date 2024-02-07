1dde393b9eed57b5a2a1f78eaa68b6d698674539
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2024-02-07 11:34:17 +0100

    merge avec Maysa

--- Map/house3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="house3" tilewidth="16" tileheight="16" tilecount="1188" columns="54">
- <image source="house3.jpg" trans="c5b24b" width="866" height="367"/>
-</tileset>
