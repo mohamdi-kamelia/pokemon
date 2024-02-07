e0602a1c50acd6bea9cf4e6479ccc7e6d9f690ab
maysa-bik <maysa.bik@laplateforme.com>
2024-02-07 14:38:24 +0100

    90

--- fichier/Menu.py
+++ fichier/Menu.py
@@ -102,9 +102,6 @@ def main_menu():
         SCREEN.blit(resized_bg, (0, 0))
 
  
-
-
-
         MENU_MOUSE_POS = pygame.mouse.get_pos()
         MENU_TEXT = get_font(80).render("  POKEMON  ", True, "#FFD700")
         MENU_RECT = MENU_TEXT.get_rect(center=(500, 60))
