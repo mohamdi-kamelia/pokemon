47dbda84bbdbca3f5568a5de6a826a39718ce1e7
SOPHIA FRAOUET <147490310+sofia-fraouet@users.noreply.github.com>
2024-02-12 18:50:56 +0100

    up to date ?

--- pokedexgui.py
+++ pokedexgui.py
@@ -4,8 +4,7 @@ import json
 pygame.init()
 
 
-with open("pokedex.json", "r") as file:
-       pokemon_data = json.load(file)
+
 
 
 width, height = 800, 600
@@ -25,9 +24,73 @@ font = pygame.font.Font(None, 36)
 
 def display_pokemon(pokemon_id):
     # Afficher les informations du Pokémon sur l'écran
-        text = font.render(f"{pokemon_data[str(pokemon_id)]['name']} - {pokemon_data[str(pokemon_id)]['type']}", True)
+        print(pokemon_data, "test")
+        text = font.render(f"{pokemon_data[(pokemon_id)]['Nom']} - {pokemon_data[(pokemon_id)]['type']}", True)
         screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
         pygame.display.flip()
+display_pokemon(1)
+
+
+
+
+class Pokedex:
+    def __init__(self, save:str=None):
+        self.data = [None] * 24
+        if save is None:
+            with open("pokedex.json", 'r', encoding='utf-8') as f:
+                pokdata = json.load(f)
+                for k in range(18, 24):
+                    self.data[k] = pokdata[k]
+            self.save_dex()
+        else:
+            with open(save, 'r', encoding='utf-8') as f:
+                self.data = json.load(f)
+    
+    def read_dex(self):
+        data = []
+        for k in range(len(self.data)):
+            if self.data[k] is not None:
+                data.append(self.data[k])
+        return data
+    
+    def add_pokemon(self, id_pok: int):
+        if isinstance(id_pok, int):
+            with open('pokedex.json', 'r', encoding='utf-8') as f:
+                pokdata = json.load(f)
+                self.data[id_pok] = pokdata[id_pok]
+            self.save_dex()
+    
+    def save_dex(self):
+        with open('pokemon.json', 'w') as f:
+                json.dump(self.data, f, indent=2)
+
+    
+
+
+
+    def display_pokemon(pokemon_id):
+            with open("pokedex.json", "r") as file:
+                pokemon_data = json.load(file)
+
+    # Afficher les informations du Pokémon sur l'écran
+    text = font.render(f"{pokemon_data[(pokemon_id)]['Nom']} - {pokemon_data[(pokemon_id)]['type']}", True)
+    screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
+    pygame.display.flip()
+display_pokemon(1)
+
+
+if __name__ == '__main__':
+    dex = Pokedex()
+    dexc = Pokedex(save='pokedex.json')
+    dex.add_pokemon(1)
+    # Code pour utiliser la sauvegarde dex = Pokedex(save='pokemon.json')
+    print(dex.read_dex())
+
+
+
+
+
+
 
 # Boucle principale
 running = True
@@ -51,6 +114,14 @@ while running:
     # Afficher le fond d'écran
     screen.blit(background, (0, 0))
     pygame.display.flip()
+
+
+
+
+
+
+
+
 
 #Quitter Pygame
 pygame.quit()
